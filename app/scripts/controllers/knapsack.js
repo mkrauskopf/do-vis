10ae81870820e16cb022e086239815aabd3e8721
Martin Krauskopf <martin.krauskopf@gmail.com>
2014-01-11 19:31:52 +0100

    Prototyping visualization of BaB algorithm with d3
    Based on the following jsFiddle using D3 v2:
    
      http://jsfiddle.net/mccannf/8ny7w/19/
    
    So far just very raw version which visualize visited nodes.

--- app/scripts/controllers/knapsack.js
+++ app/scripts/controllers/knapsack.js
@@ -62,7 +62,7 @@ angular.module('doVisApp')
     /** Resets the solution. */
     $scope.reset = function() {
       delete $scope.solution;
-      angular.forEach($scope.ksItems, function(value, key) {
+      angular.forEach($scope.ksItems, function(value) {
         value.selected = false;
       });
     };
@@ -70,11 +70,149 @@ angular.module('doVisApp')
     /** Solves knapsack using current items. */
     $scope.solveBaB = function() {
       $scope.reset();
-      $scope.solution = solveBaB($scope.ksCapacity, $scope.ksItems);
+      var babCallback = visualize();
+      $scope.solution = solveBaB($scope.ksCapacity, $scope.ksItems, babCallback);
       angular.forEach($scope.solution.taken, function(value, key) {
         $scope.gridOptions.selectRow(key, value);
         $scope.ksItems[key].selected = value;
       });
+    };
+
+    var visualize = function() {
+
+      var w = 960,
+          h = 500,
+          root = { id: '' },
+          data = [root],
+          tree = d3.layout.tree().size([w - 20, h - 20]),
+          diagonal = d3.svg.diagonal(),
+          duration = 400;
+
+      var vis = d3.select('#babViz').append('svg:svg')
+          .attr('width', w)
+          .attr('height', h)
+        .append('svg:g')
+          .attr('transform', 'translate(10, 10)');
+
+      vis.selectAll('circle.node')
+          .data(tree(root))
+        .enter().append('svg:circle')
+          .attr('class', 'node')
+          .attr('r', 3.5)
+          .attr('cx', x)
+          .attr('cy', y);
+
+      vis.selectAll('text.node')
+          .data(tree(root))
+        .enter().append('svg:text')
+          .attr('class', 'node')
+          .text(function(d) { return ''; })
+          .attr('x', x)
+          .attr('y', y)
+          .attr('dx', 4)
+          .attr('dy', 4); //padding-left and padding-top
+
+      var update = function(nodeName) {
+        // Add a new datum to a random parent.
+        //var n = ~~(Math.random() * data.length);
+        //var d = {id: data.length}, parent = data[n];
+        var parentId = nodeName.slice(0, -1);
+        var parent;
+        for (var i in data) {
+          if (data[i].id === parentId) {
+            parent = data[i];
+            break;
+          }
+        }
+        var currentNode = {id: nodeName};
+        if (parent.children) {
+          parent.children.push(currentNode);
+        } else {
+          parent.children = [currentNode];
+        }
+        data.push(currentNode);
+
+        // Compute the new tree layout. We'll stash the old layout in the data.
+        var nodes = tree(root);
+
+        // Update the nodes…
+        var cnode = vis.selectAll('circle.node')
+              .data(nodes, nodeId);
+
+        cnode.enter().append('svg:circle')
+              .attr('class', 'node')
+              .attr('r', 3.5)
+              .attr('cx', function(d) { return d.parent.data.x0; })
+              .attr('cy', function(d) { return d.parent.data.y0; })
+            .transition()
+              .duration(duration)
+              .attr('cx', x)
+              .attr('cy', y);
+
+        var tnode = vis.selectAll('text.node')
+              .data(nodes, nodeId);
+
+        tnode.enter().append('svg:text')
+              .attr('class', 'node')
+              .text(function(d) { return nodeName; })
+              .attr('x', function(d) { return d.parent.data.x0; })
+              .attr('y', function(d) { return d.parent.data.y0; })
+            .transition()
+              .duration(duration)
+              .attr('x', x)
+              .attr('y', y);
+
+        // Transition nodes to their new position.
+        cnode.transition()
+            .duration(duration)
+            .attr('cx', x)
+            .attr('cy', y);
+
+        tnode.transition()
+            .duration(duration)
+            .attr('x', x)
+            .attr('y', y)
+            .attr('dx', 4)
+            .attr('dy', 4); //padding-left and padding-top
+
+        // Update the links…
+        var link = vis.selectAll('path.link')
+            .data(tree.links(nodes), linkId);
+
+        // Enter any new links at the parent's previous position.
+        link.enter().insert('svg:path', 'g.node')
+            .attr('class', 'link')
+            .attr('d', function(d) {
+              var o = {x: d.source.data.x0, y: d.source.data.y0};
+              return diagonal({source: o, target: o});
+            })
+          .transition()
+            .duration(duration)
+            .attr('d', diagonal);
+
+        // Transition links to their new position.
+        link.transition()
+            .duration(duration)
+            .attr('d', diagonal);
+      };
+
+      function linkId(d) {
+        return d.source.data.id + '-' + d.target.data.id;
+      }
+
+      function nodeId(d) {
+        return d.data.id;
+      }
+
+      function x(d) {
+        return d.data.x0 = d.x;
+      }
+
+      function y(d) {
+        return d.data.y0 = d.y;
+      }
+
+      return update;
     };
   }
 );
--- app/scripts/libs/knapsack.js
+++ app/scripts/libs/knapsack.js
@@ -2,8 +2,32 @@
 
 var _items = [];
 
-function doSolve(solution, value, remainingCap, maxEstimate, taken, level) {
+function doSolve(solution, value, remainingCap, maxEstimate, taken, level, callback) {
+
+  function updateSolution(solution, value, taken) {
+    if (solution.value < value) {
+      // console.log("Having new solution. Value: " + value + ", taken: " + taken);
+      solution.value = value;
+      solution.taken = taken.slice();
+    }
+  }
+
+  /*
+  console.log("Trying: " + JSON.stringify({
+    "value": value,
+    "remainingCap": remainingCap,
+    "maxEstimate": maxEstimate,
+    "taken": taken,
+    "level": level,
+    "solution": solution
+  }));
+  */
+  if (taken.length > 0) {
+    var nodeName = taken.join('');
+    callback(nodeName);
+  }
   if (maxEstimate < solution.value) {
+    // console.log("skipping due to insuffient solution.value")
     return;
   }
   if (level >= _items.length) {
@@ -15,25 +39,19 @@ function doSolve(solution, value, remainingCap, maxEstimate, taken, level) {
   if (exceedCap) { // capacity exceeded. Prune whole subtree under current node.
     taken[level] = 0;
     updateSolution(solution, value, taken);
-    doSolve(solution, value, remainingCap, maxEstimate - item.value, taken, level + 1);
+    doSolve(solution, value, remainingCap, maxEstimate - item.value, taken, level + 1, callback);
   } else {
     // use it
     taken[level] = 1;
-    doSolve(solution, value + item.value, remainingCap - item.weight, maxEstimate, taken, level + 1);
+    doSolve(solution, value + item.value, remainingCap - item.weight, maxEstimate, taken, level + 1, callback);
     // do not use it
     taken[level] = 0;
-    doSolve(solution, value, remainingCap, maxEstimate - item.value, taken, level + 1);
+    doSolve(solution, value, remainingCap, maxEstimate - item.value, taken, level + 1, callback);
   }
-
-  function updateSolution(solution, value, taken) {
-    if (solution.value < value) {
-      solution.value = value;
-      solution.taken = taken.slice();
-    }
-  }
+  taken.pop();
 }
 
-function solveBaB(capacity, items) {
+function solveBaB(capacity, items, callback) {
   var maxEstimate = 0;
   _items = items;
   $.each(items, function () {
@@ -45,7 +63,7 @@ function solveBaB(capacity, items) {
     taken: [],
   };
 
-  doSolve(solution, 0, capacity, maxEstimate, [], 0);
+  doSolve(solution, 0, capacity, maxEstimate, [], 0, callback);
   return solution;
 }
 
